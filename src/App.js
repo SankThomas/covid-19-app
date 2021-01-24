a9178405e4b9ee1b785badaf356c78e9b08a91f6
Thomas Sankara <thomassankara74@gmail.com>
2021-01-24 09:13:45 +0300

    disable search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Countries from './components/Countries'
-import Search from './components/Search'
+// import Search from './components/Search'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Search />
+      {/* <Search /> */}
       <Countries />
     </>
   )
